2746448fa07c0d5bd063c06460ba230d54f5cf10
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-10-02 17:10:06 +0300

    finishig lab 4

--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -12,36 +12,36 @@ public class App {
 
     public static void main(String[] args) {
         Bitmap bitmap = new Bitmap();
-//        bitmap.mirrorImage(new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg")
-//                , new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageMirror.jpg"));
-//        bitmap.rotate90(new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg")
-//        , new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageRotate.jpg"),
-//                1);
-//        bitmap.darken(new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"),
-//                new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageDarken.jpg") );
+        bitmap.mirrorImage(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg")
+                , new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageMirror.jpg"));
+        bitmap.rotate90(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg")
+        , new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageRotate.jpg"),
+                1);
+        bitmap.darken(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"),
+                new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageDarken.jpg") );
+
+        bitmap.lighten(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"),
+                new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageLighten.jpg") );
+
+//        String imageTransformation = args[2];
+//        File sourceImageFile = new File(args[0]);
+//        File targetImageFile = new File(args[1]);
 //
-//        bitmap.lighten(new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"),
-//                new File("C:\\Users\\STUDENT\\java-401\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageLighter.jpg") );
-
-        String imageTransformation = args[2];
-        File sourceImageFile = new File(args[0]);
-        File targetImageFile = new File(args[1]);
-
-        if (imageTransformation.contains("rotate")) {
-            if (imageTransformation.endsWith("+1"))
-                bitmap.rotate90(sourceImageFile, targetImageFile, 1);
-            else if (imageTransformation.endsWith("-1"))
-                bitmap.rotate90(sourceImageFile, targetImageFile, -1);
-            else
-                System.out.println("Direction of rotation either 1 or -1 (Left || Right)");
-        } else if (imageTransformation.equals("mirror"))
-            bitmap.mirrorImage(sourceImageFile, targetImageFile);
-        else if (imageTransformation.equals("darken"))
-            bitmap.darken(sourceImageFile, targetImageFile);
-        else if (imageTransformation.equals("lighten"))
-            bitmap.lighten(sourceImageFile, targetImageFile);
-        else
-            System.out.println("Invalid CLI input please enter a valid inputs");
+//        if (imageTransformation.contains("rotate")) {
+//            if (imageTransformation.endsWith("+1"))
+//                bitmap.rotate90(sourceImageFile, targetImageFile, 1);
+//            else if (imageTransformation.endsWith("-1"))
+//                bitmap.rotate90(sourceImageFile, targetImageFile, -1);
+//            else
+//                System.out.println("Direction of rotation either 1 or -1 (Left || Right)");
+//        } else if (imageTransformation.equals("mirror"))
+//            bitmap.mirrorImage(sourceImageFile, targetImageFile);
+//        else if (imageTransformation.equals("darken"))
+//            bitmap.darken(sourceImageFile, targetImageFile);
+//        else if (imageTransformation.equals("lighten"))
+//            bitmap.lighten(sourceImageFile, targetImageFile);
+//        else
+//            System.out.println("Invalid CLI input please enter a valid inputs");
 
     }
 }
--- app/src/test/java/bitmap/transformer/AppTest.java
+++ app/src/test/java/bitmap/transformer/AppTest.java
@@ -5,7 +5,82 @@ package bitmap.transformer;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 class AppTest {
 
+    @Test
+    public void mirrorImage() {
+        Bitmap bitmap = new Bitmap();
+        bitmap.mirrorImage(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"),
+                new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageMirror.jpg"));
+        try {
+            BufferedImage soursImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"));
+            BufferedImage targetImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageMirror.jpg"));
+            int soursWidth = soursImage.getWidth();
+            int targetWidth = targetImage.getWidth();
+            assertEquals(soursWidth*2 ,targetWidth,"Target Image Width should be double the source Image Width (targetWidth = soursWidth*2 )." );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void rotate90(){
+        Bitmap bitmap = new Bitmap();
+        bitmap.rotate90(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"),
+                new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageRotate.jpg") , +1);
+        try {
+            BufferedImage soursImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_image.jpg"));
+            BufferedImage targetImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\GUI_it_imageRotate.jpg"));
+            int soursWidth = soursImage.getWidth();
+            int targetHeight = targetImage.getHeight();
+            assertEquals(soursWidth ,targetHeight,"Target Image Height should be the same as the source Image Width (targetHeight = soursWidth )." );
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void darken(){
+        Bitmap bitmap = new Bitmap();
+        bitmap.darken(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImage.bmp"),
+                new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImageDarken.bmp"));
+        try {
+            BufferedImage soursImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImage.bmp"));
+            BufferedImage targetImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImageDarken.bmp"));
+            Color rgb = new Color(soursImage.getRGB(0,0));
+            rgb = rgb.darker();
+            Color rgbDarken = new Color(targetImage.getRGB(0,0));
+            assertEquals(rgb.getRGB(),rgbDarken.getRGB() ,"The first pixel in the target image should be darker than the The first pixel in the source image");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void lighten(){
+        Bitmap bitmap = new Bitmap();
+        bitmap.lighten(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImage.bmp"),
+                new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImageLighten.bmp"));
+        try {
+            BufferedImage soursImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImage.bmp"));
+            BufferedImage targetImage = ImageIO.read(new File("C:\\Users\\STUDENT\\401courseJAVA\\java-fundamentals\\bitmap-transformer\\app\\src\\main\\resources\\BmpImageLighten.bmp"));
+            Color rgb = new Color(soursImage.getRGB(0,0));
+            rgb = rgb.brighter();
+            Color rgbDarken = new Color(targetImage.getRGB(0,0));
+            assertEquals(rgb.getRGB(),rgbDarken.getRGB(),"The first pixel in the target image should be brighter than the The first pixel in the source image");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
